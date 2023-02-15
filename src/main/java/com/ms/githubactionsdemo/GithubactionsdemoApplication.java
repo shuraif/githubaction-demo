2a3d28fa7cab89bd313eb653d63883a807d07496
shuraif <shuraifpilathara@gmail.com>
2023-02-15 17:34:05 +0530

    Initial commit

---
+++ src/main/java/com/ms/githubactionsdemo/GithubactionsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.ms.githubactionsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubactionsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubactionsdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/githubactionsdemo/GithubactionsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.githubactionsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubactionsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
